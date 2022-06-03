19c2d1df9c685890f6ca7174c1e5bb7de7ec61c2
Stefanija Duracoska <86681270+stefanijAaA@users.noreply.github.com>
2022-06-03 23:48:39 +0200

    Update SILab2Test.java

--- SI_lab2_206008/src/test/java/SILab2Test.java
+++ SI_lab2_206008/src/test/java/SILab2Test.java
@@ -17,5 +17,17 @@ public class SILab2Test {
 
         assertEquals(List.of("1","1","1","#","#","#","#","3","#"), SILab2.function(List.of("0","0","0","#","#","#","#","0","#")));
     }
+     @Test
+    void EveryBranch(){
+
+        IllegalArgumentException ex = assertThrows(IllegalArgumentException.class, () -> SILab2.function(new ArrayList<>()));
+        assertTrue(ex.getMessage().contains("List length should be greater than 0"));
+
+        IllegalArgumentException ex1 = assertThrows(IllegalArgumentException.class, () -> SILab2.function(List.of("#","0","0")));
+        assertTrue(ex1.getMessage().contains("List length should be a perfect square"));
+
+        assertEquals(List.of("1","1","1","#","#","#","#","3","#"), SILab2.function(List.of("0","0","0","#","#","#","#","0","#")));
+    }
+
 
 }
